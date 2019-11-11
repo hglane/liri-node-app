4ba26cd930a76f6ec8f9230ece1111200b2d2072
bama <hglane@crimson.ua.edu>
2019-11-10 20:20:47 -0600

    Adding txt to liri.js

--- assets/liri.js
+++ assets/liri.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
+var request = require("request")
+var Spotify = require("node-spotify-api")
+var dateFormat = require("dateFormat")
+var fs = require("fs")
