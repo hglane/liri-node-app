3270b61d5f47ff980e9d597f3aa7f72c0c84f49d
Harris <hglane@crimson.ua.edu>
2020-02-09 21:46:43 -0600

    working on getting the liri file up and running

--- assets/liri.js
+++ assets/liri.js
@@ -1,6 +1,40 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var request = require("request")
+var axios = require("axios")
 var Spotify = require("node-spotify-api")
-var dateFormat = require("dateFormat")
+var dateFormat = require("moment")
 var fs = require("fs")
+var spotify = new Spotify(keys.spotify);
+
+var getArtistNames = function(artist) {
+    return artist.name
+};
+
+var getMeSpotify = function(songName) {
+    if (songName === undefined) {
+        songName = "What's my age again?";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: songName
+        },
+        function(err, data) {
+            if (err) {
+                console.log("Error occured: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album " + songs[i].album.name);
+                console.log("----------------------------");
+            }
+        }
+    );
+};
