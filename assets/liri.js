86373ef496bd23ee667fe8efdc9b08f4026b0749
Harris <hglane@crimson.ua.edu>
2020-02-09 23:12:05 -0600

    Doing what it says

--- assets/liri.js
+++ assets/liri.js
@@ -96,6 +96,20 @@ var getMeMovie = function (movieName) {
     );
 };
 
+var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      console.log(data);
+  
+      var dataArr = data.split(",");
+  
+      if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
+      } else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+      }
+    });
+  };  
+  
 var pick = function (caseData, functionData) {
     switch (caseData) {
     case "concert-this":
